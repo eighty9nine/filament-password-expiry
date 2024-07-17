c319c49fdbaa2d7e678c947c01fba921c06091f8
John Mwakalinga <mwakalingajohn@gmail.com>
2024-07-17 17:57:03 +0300

    Adds the password expiry when the user is created

--- src/Concerns/HasPasswordExpiry.php
+++ src/Concerns/HasPasswordExpiry.php
@@ -9,8 +9,6 @@ trait HasPasswordExpiry
 {
     public static function bootHasPasswordExpiry()
     {
-
-        // check if both required columns exist in the database
         if (!Schema::hasColumn(config('password-expiry.table_name'), config('password-expiry.column_name'))) {
             throw new ColumnsNotFoundException(__('password-expiry::password-expiry.reset-password.exceptions.column_not_found', [
                 'column_name' => config('password-expiry.column_name'),
@@ -18,5 +16,15 @@ trait HasPasswordExpiry
                 'table_name' => config('password-expiry.table_name'),
             ]));
         }
+
+        static::created(function ($model) {
+            if(
+                $model->wasChanged(config('password-expiry.password_column_name')) &&
+                filled($model->{config('password-expiry.password_column_name')})
+            ) {
+                $model->{config('password-expiry.column_name')} = now()->addDays(config('password-expiry.expires_in'));
+                $model->save();
+            }
+        });
     }
 }
